405ffdfb12c5e8040143b1f55c20d2e9f6c9918a
Claudio Zizza <claudio@budgegeria.de>
2017-03-20 23:11:50 +0100

    Add test for BeanFactoryAwareTrait

---
+++ tests/bitExpert/Disco/BeanFactoryAwareTraitUnitTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Disco package.
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace bitExpert\Disco;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for {@link \bitExpert\Disco\BeanFactoryAwareTrait}.
+ */
+class BeanFactoryAwareTraitUnitTest extends TestCase
+{
+    public function testSetBeanFactory()
+    {
+        $beanFactory = $this->createMock(BeanFactory::class);
+        $trait = $this->getMockForTrait(BeanFactoryAwareTrait::class);
+
+        $trait->setBeanFactory($beanFactory);
+
+        self::assertAttributeInstanceOf(BeanFactory::class, 'beanFactory', $trait);
+        self::assertAttributeSame($beanFactory, 'beanFactory', $trait);
+    }
+}
